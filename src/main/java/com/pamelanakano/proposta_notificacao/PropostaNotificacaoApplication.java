128b74e5a9455caa874f94fefeb27be9d9957505
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-19 15:39:59 -0300

    init

---
+++ src/main/java/com/pamelanakano/proposta_notificacao/PropostaNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.pamelanakano.proposta_notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamelanakano/proposta_notificacao/PropostaNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamelanakano.proposta_notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
