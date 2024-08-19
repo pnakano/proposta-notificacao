4ee740ae3db92c3c55cb2ba712855fc0986232ec
Pamela Nakano <pamela.nakano@hotmail.com>
2024-08-19 17:48:29 -0300

    Melhoras na implementação do envio de e-mail

--- src/main/java/com/pamelanakano/proposta_notificacao/service/NotificacaoServiceImpl.java
+++ src/main/java/com/pamelanakano/proposta_notificacao/service/NotificacaoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pamelanakano.proposta_notificacao.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,21 +12,28 @@ public class NotificacaoServiceImpl implements NotificacaoService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String username;
+    private final String username;
 
-    public NotificacaoServiceImpl(JavaMailSender mailSender) {
+    private final Logger logger = LoggerFactory.getLogger(NotificacaoServiceImpl.class);
+
+    public NotificacaoServiceImpl(JavaMailSender mailSender,
+                                  @Value("${spring.mail.username}") String username) {
         this.mailSender = mailSender;
+        this.username = username;
     }
 
     public void enviarEmail(String assuntoEmail, String corpoEmail, String emailUsuario) {
 
-        var message = new SimpleMailMessage();
-        message.setFrom(username);
-        message.setTo(emailUsuario);
-        message.setSubject(assuntoEmail);
-        message.setText(corpoEmail);
-        mailSender.send(message);
+        try {
+            var message = new SimpleMailMessage();
+            message.setFrom(username);
+            message.setTo(emailUsuario);
+            message.setSubject(assuntoEmail);
+            message.setText(corpoEmail);
+            mailSender.send(message);
+        } catch (Exception e) {
+            logger.error("Erro ao enviar e-mail: " + e.getMessage());
+        }
 
     }
 
